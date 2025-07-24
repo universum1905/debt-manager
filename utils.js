5904f9a97ffbf8e74257885173e93e87804987af
universum1905 <evren-oezerkan@web.de>
2025-07-24 12:53:52 +0200

    1

--- utils.js
+++
@@ -1,7 +0,0 @@
-function convertCurrency(amount, toEUR = false, rate = 46) {
-    return toEUR ? (amount / rate).toFixed(2) + " €" : (amount * rate).toFixed(0) + " TL";
-}
-
-function calculateInterest(balance, rate) {
-    return balance * (rate / 100);
-}
