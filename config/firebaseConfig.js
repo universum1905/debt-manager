17bd876b84ced8ccd8ccaeb1934eb9c27da2e802
universum1905 <evren-oezerkan@web.de>
2025-07-24 12:26:40 +0200

    Delete firebaseConfig.js

--- config/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "HIER_EINTRAGEN",
-  authDomain: "HIER_EINTRAGEN",
-  projectId: "HIER_EINTRAGEN",
-  storageBucket: "HIER_EINTRAGEN",
-  messagingSenderId: "HIER_EINTRAGEN",
-  appId: "HIER_EINTRAGEN"
-};
-firebase.initializeApp(firebaseConfig);
